065e4a6773e480d98eb31a5010275deec4245a4f
Praneeth Ravuri <prav2500@gmail.com>
2023-06-29 10:01:41 +0530

    Simple terminal inputs

--- src/components/Command.vue
+++ src/components/Command.vue
@@ -1,94 +1,61 @@
 <template>
-    <div class="terminal">
-        <div class="terminal-output" ref="outputContainer">
-            <div v-for="message in messages" :key="message.id" class="terminal-message">
-                <span><Prompt /><span class="terminal-text" v-html="message.text"></span></span>
+    <div class="terminal-input">
+
+        <div class="show-previous-commands">
+            <div v-for="(command, i) in storeCommand" :key="i">
+                <span>
+                    <Prompt />{{ command }}
+                </span>
+
+                <div v-if="command === 'help'">
+                    <Help />
+                </div>
+                <div v-if="command === 'projects'">
+                    <Projects />
+                </div>
+
             </div>
         </div>
-        <div class="terminal-input">
-            <component :is="PromptComponent"></component>
-            <input v-model="currentInput" @keyup.enter="processInput" autofocus />
-        </div>
-        <component v-if="showProjects" :is="Projects"></component>
-        <component v-if="showHelp" :is="Help"></component>
+
+        <label for="prompt">
+            <Prompt />
+        </label>
+        <input type="text" @keyup.enter="displayCommandOutput" autofocus v-model="inputValue">
     </div>
 </template>
   
-  
 <script>
-import { ref, reactive, nextTick } from 'vue';
-
-import Projects from './Projects.vue'; // Import your Projects component here
+import Projects from './Projects.vue';
 import Prompt from './Prompt.vue';
 import Help from './Help.vue';
 
 export default {
-    setup() {
-        const messages = reactive([]);
-        const currentInput = ref('');
-        const showProjects = ref(false);
-        const showHelp = ref(false);
-
-        const processInput = () => {
-            const input = currentInput.value.trim();
-
-            if (input !== '') {
-                // Process the input and generate the appropriate component
-                const generatedComponent = generateComponent(input);
-
-                // Add the input and the generated component to the terminal's messages
-                messages.push({ id: Date.now() + 1, prompt: '', text: generatedComponent });
-
-                // Clear the input field
-                currentInput.value = '';
-
-                // Scroll to the bottom of the output container
-                nextTick(() => {
-                    const outputContainer = outputContainerRef.value;
-                    outputContainer.scrollTop = outputContainer.scrollHeight;
-                });
-            }
-        };
-
-        const outputContainerRef = ref(null);
-        const PromptComponent = Prompt;
-
-        const generateComponent = (input) => {
-            // Logic to generate the component based on the input
-            // Replace this with your own logic
-
-            if (input === 'projects') {
-                showProjects.value = true; // Show the Projects component
-                return ''; // Return an empty string as the generated component
-            } else if (input === 'help') {
-                showHelp.value = true;
-            } else if (input === 'image') {
-                return '<img src="path/to/image.jpg" alt="Generated Image">';
-            } else {
-                return `Unknown command: ${input}`;
-            }
-        };
-
+    name: "Command",
+    components: {
+        Help,
+        Prompt,
+        Projects,
+    },
+    data() {
         return {
-            messages,
-            currentInput,
-            showProjects,
-            showHelp,
-            processInput,
-            outputContainerRef,
-            Projects,
-            Help,
-            PromptComponent
+            inputValue: '',
+            currentTab: '',
+            storeCommand: [],
+            correctCommands: ["help", "projects"]
+
         };
     },
-    components: {
-        Projects,
-        Prompt,
-        Help // Register Projects component
-    }
+    methods: {
+        displayCommandOutput() {
+            if (this.correctCommands.includes(this.inputValue)) {
+                this.storeCommand.push(this.inputValue);
+            } else {
+                // Default to Home tab if input doesn't match any condition
+                console.log("invalid option");
+            }
+            this.inputValue = '';
+        },
+    },
 };
 </script>
   
-<style>
-/* CSS styles remain the same */
-</style>
