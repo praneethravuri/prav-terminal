1730490bc962289416f1f04f32a210426cacf032
Praneeth Ravuri <prav2500@gmail.com>
2023-07-01 16:27:52 +0530

    Delete setFocus.js

--- src/setFocus.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    document.getElementsByTagName("body")[0].addEventListener("keypress", () => {
-        
-        document.getElementById("command-input").focus();
-    });
-});
