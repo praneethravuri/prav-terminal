f2aad91857ac555443843c92ff0619c45a505014
Praneeth Ravuri <prav2500@gmail.com>
2023-07-03 20:31:55 +0530

    PWA implementation

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,25 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from "node:url"
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      manifest : {
+        icons : [
+          {
+            src : "/favicon.ico",
+            sizes : "100x100",
+            type : "image/ico",
+            purpose : "any maskable"
+          }
+        ]
+      },
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL("./src", import.meta.url))
