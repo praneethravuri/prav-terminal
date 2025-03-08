c33fa49a2cf1bff5b543f908df0785c67dd6861e
Praneeth Ravuri <pravdevrav@gmail.com>
2025-03-08 14:14:50 -0600

    Update Experience.vue

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -23,7 +23,7 @@ export default {
                     company: "Prodapt",
                     position: "Software Engineer",
                     location: "Richardson, Texas (On-site)",
-                    dateRange: "Jul. 2024 - Present",
+                    dateRange: "Oct. 2024 - Present",
                     content: [
                         "At Prodapt, I took on the challenge of managing telemetry data from over 5,000 network devices. Picture 5,000 people talking all at once—it's chaos unless you know how to listen. Using Go and Kafka, I built a system that could handle this chatter and make sense of it.",
                         "One of my key tasks was configuring a decoder to handle enterprise-specific data for over 170 network metrics. It wasn't built from scratch, but I made sure it worked perfectly for our needs. I also added a safety net—if the database ever went down, the system would save data to files so we wouldn't lose anything critical.",
@@ -32,9 +32,9 @@ export default {
                 },
                 {
                     company: "Cognizant",
-                    position: "Software Engineer",
+                    position: "Software Engineer Intern",
                     location: "Hyderabad, India",
-                    dateRange: "Jan. 2020 - Jun. 2022",
+                    dateRange: "Jan. 2022 - Jun. 2022",
                     content: [
                         "At Cognizant, I worked on making real-time communication simple and seamless. I built a platform using WebSocket protocols that could handle 500+ users chatting at once—like hosting a giant virtual event where everyone's conversations flowed smoothly, no lag or hiccups.",
                         "I also rolled up my sleeves to improve API performance. By adding Redis caching and connection pooling, I sped up response times big time. Imagine turning a crowded highway into one with express lanes—everything moves faster, and frequent users get even quicker access.",
